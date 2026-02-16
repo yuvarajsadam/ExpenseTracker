48c0d95454331c5f57010a312d4f40a7188b624d
yuvarajsadam <yuvarajsadam006@gmail.com>
2026-02-16 22:04:23 +0530

    Update API base URL to production endpoint

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://expensetracker-4l3q.onrender.com/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
